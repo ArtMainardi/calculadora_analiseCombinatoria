fa8213fb71f8d8212c88158fd767bfc5e049370f
Art_Mainardi <iensenmainardi@gmail.com>
2026-05-15 09:56:21 -0300

    Implementação Básica do Sistema de Anagramas

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,17 @@ import java.util.Scanner;
 public class Main {
     private static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
+        System.out.println("Digite uma palavra: ");
+        String palavra = sc.nextLine();
 
+        // Calculando Permutação
+        int tamanho = palavra.length(), permutacao = tamanho;
+
+        while((tamanho-1) > 0){
+            tamanho--;
+            permutacao *= tamanho;
+        }
+
+        System.out.println("Quantidade de anagramas: " + permutacao);
     }
 }
