b53e7cd01e9123cf2ff52ad1875b739736eb1292
ARTHUR IENSEN MAINARDI <arthur_mainardi@estudante.sesisenai.org.br>
2026-06-01 07:49:59 -0300

    Separação da main com as outras funções

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    // Variáveis Globais:
-    private static Scanner sc = new Scanner(System.in);
-
     // Menu inicial:
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int option;
 
         do{
@@ -19,9 +16,10 @@ public class Main {
             );
             option = sc.nextInt();
 
+            Anagrama an = new Anagrama();
             switch(option){
                 case 1:
-                    anagrama();
+                    an.anagrama();
                     break;
                 case 0:
                     break;
@@ -30,49 +28,5 @@ public class Main {
                     System.out.println(); // Espaçamento
             }
         } while(option != 0);
-    }
-
-    // Procedimento do Anagrama:
-    public static void anagrama(){
-        // Entrada de dados:
-        System.out.println("Digite uma palavra: ");
-        sc.nextLine();
-        String palavra = sc.nextLine();
-
-        // Verificando letras repetidas:
-        int tamanho = 0;
-        ArrayList<Character> letras = new ArrayList<>();
-        for(int cont = 0; cont < (palavra.length()-1); cont++){
-            if(!letras.isEmpty()){
-                boolean verify = true;
-
-                for(int cont2 = 0; cont2 < letras.size(); cont2++){
-                    if(palavra.toLowerCase().charAt(cont) == letras.get(cont2)){
-                        verify = false;
-                        System.out.println("Letra REPETIDA!!  " + cont);
-                    }
-                }
-
-                if(verify){
-                    tamanho++;
-                    letras.add(palavra.toLowerCase().charAt(cont));
-                }
-            } else{
-                tamanho++;
-                letras.add(palavra.toLowerCase().charAt(cont));
-            }
-        }
-        System.out.println("Quantidade de letras únicas: " + tamanho);
-
-        // Calculando Permutação:
-        int permutacao = tamanho;
-        while((tamanho-1) != 0){
-            tamanho--;
-            permutacao *= tamanho;
-        }
-
-        // Saída de dados:
-        System.out.println("Quantidade de anagramas: " + permutacao);
-        System.out.println("\n"); // Espaçamento
     }
 }
