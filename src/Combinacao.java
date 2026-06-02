a35fca28d454a4c93b933fa92631c4ad95c9fd44
MATHEUS LACERDA <matheus_lacerda1@estudante.sesisenai.org.br>
2026-06-02 09:03:08 -0300

    Combinação agora simplifica o cálculo

--- src/Combinacao.java
+++ src/Combinacao.java
@@ -8,10 +8,14 @@ public class Combinacao {
         int n = sc.nextInt();
         System.out.println("Insira a quantidade de elementos desejados para a escolha: (Precisa ser menor ou igual ao número total de elementos disponíveis)");
         int p = sc.nextInt();
-        if(p <= n){
-            return per.p(n) / (per.p(p) * per.p(n - p));
+        if(p <= n) {
+            Long res = 1L;
+            for (int i = (n - p + 1); i <= n; i++) {
+                res *= i;
+            }
+            return res / per.p(p);
         }
-        System.out.println("ERRO! Retornando 0...");
+        System.out.println("ERRO! P não pode ser maior que N, retornando 0...");
         return 0L;
     }
 }
